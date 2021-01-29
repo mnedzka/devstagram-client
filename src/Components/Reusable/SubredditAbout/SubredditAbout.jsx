d9b4c9d3d6a5bbb7b526b130d208e2b23df40421
Joshuatauro <joshuatauro45@gmail.com>
2021-01-29 17:38:06 +0400

    Added ability to follow subreddits

--- src/Components/Reusable/SubredditAbout/SubredditAbout.jsx
+++ src/Components/Reusable/SubredditAbout/SubredditAbout.jsx
@@ -10,16 +10,27 @@ const SubredditAbout = ({numOfPosts}) => {
   const {currentUser} =  useContext(AuthContext)
   const { subreddit } = useParams()
 
+  const [isFollowing, setIsFollowing] = useState(false)
+
   const [about, setAbout] = useState('')
 
   const followSubreddit = async() => {
-    console.log('object')
     const formattedDetails = {
       username: await currentUser.displayName,
       subreddit
     }
     const follow = await axios.post('http://localhost:5000/user/follow/subreddit', formattedDetails)
-    console.log(follow)
+    setIsFollowing(true)
+  }
+
+  const unfollowSubreddit = async() => {
+    const formattedDetails = {
+      username: await currentUser.displayName,
+      subreddit
+    }
+    const unfollow = await axios.post('http://localhost:5000/user/unfollow/subreddit', formattedDetails)
+    setIsFollowing(false)
+
   }
   
 
@@ -27,6 +38,8 @@ const SubredditAbout = ({numOfPosts}) => {
     const fetchSubredditDetails = async() => {
       const fetchDetails = await axios.get(`http://localhost:5000/subreddits/${subreddit}`)
       setAbout(fetchDetails.data.data.about)
+      // setFollowedBy(fetchDetails.data.data.followed_by)
+      setIsFollowing(false)
     }
 
     fetchSubredditDetails()
@@ -37,9 +50,16 @@ const SubredditAbout = ({numOfPosts}) => {
         <h1 className="subreddit_about__title">TP/{subreddit}</h1>
         <p className="subreddit_about__para">{about}</p>
         <h3 className="subreddit_about__posts"><span className="posts__num">{numOfPosts}</span> Posts</h3>
-        <div className="" onClick={followSubreddit}>
-          <ActionButton buttonColor="blue" buttonSize="lg" buttonText="Join" buttonVariant="filled" />
-        </div>
+        
+        { isFollowing ? (
+          <div className="" onClick={unfollowSubreddit}>
+            <ActionButton buttonColor="blue" buttonSize="lg" buttonText="Leave" buttonVariant="outlined" />
+          </div>
+        ) : (
+          <div className="" onClick={followSubreddit}>
+            <ActionButton buttonColor="blue" buttonSize="lg" buttonText="Join" buttonVariant="filled" />
+          </div>
+        )}
       </div>
     </section>
   )
